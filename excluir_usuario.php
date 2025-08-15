bbecefa5151467248c8ad12401cada092c8da875
Júlia Pavlick <juliapavlickk@gmail.com>
2025-08-15 16:01:15 -0300

    .

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -32,3 +32,42 @@ if($stmt->execute()) {
     echo "<script>alert('Não foi possível excluir o usuário!');</script>";
 }
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Excluir usuario</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Excluir Usuario</h2>
+    <?php if(!empyt($usuarios)):?>
+        <table border="1">
+            <tr>
+                <th>ID<th>
+                <th>Nome<th>
+                <th>Email<th>
+                <th>Perfil<th>
+                <th>Ações<th>
+            </tr>
+        <?php foreach($usuarios as $usuario): ?>
+            <tr>
+                <td><?= htmlspecialchars($usuario['id_usuario'])?></td>
+                <td><?= htmlspecialchars($usuario['nome'])?></td>
+                <td><?= htmlspecialchars($usuario['email'])?></td>
+                <td><?= htmlspecialchars($usuario['id_perfil'])?></td>
+                <td>
+                    <a href="excluir_usuario.php?id=<?= htmlspecialchars($usuario['id_usuario'])?>"
+                    onclick="return confirm('Tem certeza que deseja excluir este usuario?')">Excluir</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php else:?>
+        <p>Nenhum usuario encontrado</p>
+    <?php endif; ?>
+
+    <a href="principal.php">Voltar</a>
+</body>
+</html>
