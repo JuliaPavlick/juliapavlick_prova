ea35a7aa49480df0e381936943ff1bfd56ee1530
Júlia Pavlick <juliapavlickk@gmail.com>
2025-08-21 15:59:46 -0300

    primeiro ajuste do buscar prod

---
+++ buscar_produto.php
@@ -0,0 +1,165 @@
+<?php
+session_start();
+require_once("conexao.php");
+
+//VERIFICA SE O USUARIO TEM PERMISSAO DE adm OU secretaria]
+if ($_SESSION['perfil'] != 1 && $_SESSION['perfil'] != 2) {
+    echo "<script>alert('Acesso negado!');window.location.href='principal.php'</script>";
+    exit();
+}
+
+$produto = []; //INICIALIZA A VARIAVEL PARA EVITAR ERROS
+
+//SE O FORMULARIO FOR ENVIADO, BUSCA O NOME DO PRODUTO PELO ID OU NOME
+if ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST['busca'])) {
+    $busca = trim($_POST['busca']);
+
+    //VERIFICA SE A BUSCA É UM NUMERO OU NOME
+    if (is_numeric($busca)) {
+        $sql = "SELECT * FROM produto WHERE id_produto = :busca ORDER BY nome_prod ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':busca', $busca, PDO::PARAM_INT);
+    } else {
+        $sql = "SELECT * FROM produto WHERE nome_prod LIKE :busca_nome_prod ORDER BY nome_prod ASC";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(':busca_nome_prod', "%$busca%", PDO::PARAM_STR);
+    }
+
+} else {
+    $sql = "SELECT * FROM produto ORDER BY nome_prod ASC";
+    $stmt = $pdo->prepare($sql);
+}
+$stmt->execute();
+$usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$id_perfil = $_SESSION['perfil'];
+$permissoes = [
+    //adm
+    1 => [
+        "Cadastrar" => [
+            "cadastro_usuario.php",
+            "cadastro_perfil.php",
+            "cadastro_cliente.php",
+            "cadastro_fornecedor.php",
+            "cadastro_produto.php",
+            "cadastro_funcionario.php"
+        ],
+        "Buscar" => [
+            "buscar_usuario.php",
+            "buscar_perfil.php",
+            "buscar_cliente.php",
+            "buscar_fornecedor.php",
+            "buscar_produto.php",
+            "buscar_funcionario.php"
+        ],
+        "Alterar" => [
+            "alterar_usuario.php",
+            "alterar_perfil.php",
+            "alterar_cliente.php",
+            "alterar_fornecedor.php",
+            "alterar_produto.php",
+            "alterar_funcionario.php"
+        ],
+        "Excluir" => [
+            "excluir_usuario.php",
+            "excluir_perfil.php",
+            "excluir_cliente.php",
+            "excluir_fornecedor.php",
+            "excluir_produto.php",
+            "excluir_funcionario.php"
+        ]
+    ],
+    //secretaria
+    2 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php","buscar_usuario.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir" => ["excluir_produto.php"]
+    ],
+    //almoxarife
+    3 => [
+        "Cadastrar" => ["cadastro_fornecedor.php", "cadastro_produto.php"],
+        "Buscar" => ["buscar_cliente.php", "buscar_fornecedor.php", "buscar_produto.php"],
+        "Alterar" => ["alterar_fornecedor.php", "alterar_produto.php"],
+        "Excluir" => ["excluir_produto.php",]
+    ],
+    //cliente
+    4 => [
+        "Cadastrar" => ["cadastro_cliente.php"],
+        "Buscar" => ["buscar_produto.php"],
+        "Alterar" => ["alterar_cliente.php"],
+    ],
+];
+$opcoes_menu = $permissoes[$id_perfil];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Busca de Produtos</title>
+    <link rel="stylesheet" href="css/bu.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav>
+        <ul class="menu">
+            <?php foreach ($opcoes_menu as $categoria => $arquivos): ?>
+                <li class="dropdown">
+                    <a href="#"><?= $categoria ?></a>
+                    <ul class="dropdown-menu">
+                        <?php foreach ($arquivos as $arquivo): ?>
+                            <li>
+                                <a href="<?= $arquivo ?>"><?= ucfirst(str_replace("_", " ", basename($arquivo, ".php"))) ?></a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </nav>
+
+    <h2>Lista de Produtos</h2>
+
+    <form action="buscar_produto.php" method="POST">
+        <label for="busca">Digite o ID ou NOME(opcional): </label>
+        <input type="text" id="busca" name="busca">
+        <button type="submit">Pesquisar</button>
+    </form>
+
+    <?php if(!empty($produtos)): ?>
+
+        <table class="table">
+            <tr>
+                <th>ID</th>
+                <th>Nome do Produto</th>
+                <th>Descrição</th>
+                <th>Quantidae</th>
+                <th>Valor Unitário</th>
+            </tr>
+
+        <?php foreach($produtos as $produto): ?>
+
+            <tr>
+                <td><?=htmlspecialchars($produto['id_produto'])?> </td>
+                <td><?=htmlspecialchars($produto['nome_prod'])?> </td>
+                <td><?=htmlspecialchars($produto['descricao'])?> </td>
+                <td><?=htmlspecialchars($produto['qtde'])?> </td>
+                <td><?=htmlspecialchars($produto['valor_unit'])?> </td>
+                <td>
+                    <a href="alterar_produto.php?id=<?=htmlspecialchars($produto['id_produto'])?>">Alterar</a>
+                    <a href="excluir_produto.php?id=<?=htmlspecialchars($produto['id_produto'])?>"onclick="return confirm('Tem certeza que você deseja excluir esse produto?')">Excluir</a>
+                 </td>
+            </tr>
+            <?php endforeach;?>     
+        </table>
+        <?php else:?>
+            <p>Nenhum produto encontrado.</p>
+        <?php endif;?>   
+        <div class="text-center"> 
+        <a href="principal.php" > Voltar </a>
+        </div>
+</body>
+</html>
